11d93b5e0129331250231bc86dd522b55b3a4de5
onizawashotaro4061 <shotaro10072003@gmail.com>
2025-10-17 17:47:31 +0900

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '@/lib/supabase'
 import { useRouter } from 'next/navigation'
+export const dynamic = 'force-dynamic';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
